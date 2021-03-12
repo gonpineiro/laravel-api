da6dec4e4d79352e8fa02b418b382df83b08ab7d
Gonzalo <gon.pineiro@gmail.com>
2021-03-11 23:39:35 -0300

    [add] - Post SoftDelete

--- database/migrations/2021_03_11_224503_create_posts_table.php
+++ database/migrations/2021_03_11_224503_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
-
+            $table->softDeletes();
             $table->timestamps();
 
             /* Relaciones */
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('v1/posts', \App\Http\Controllers\Api\V1\PostController::class)
-    ->only(['show', 'index']);
+    ->only(['show', 'index', 'destroy']);

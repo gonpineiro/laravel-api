be196a0465ccf5f11831d5dd4dd1b3a954f73072
Gonzalo <gon.pineiro@gmail.com>
2021-03-12 10:38:21 -0300

    [add] - Laravel/sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 /* V1 */
-Route::apiResource('v1/posts', PostV1::class)->only(['show', 'index', 'destroy']);
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['show', 'index', 'destroy'])
+    ->middleware('auth:sanctum');
 
 /* V2 */
-Route::apiResource('v2/posts', PostV2::class)->only(['show', 'index']);
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['show', 'index'])
+    ->middleware('auth:sanctum');
